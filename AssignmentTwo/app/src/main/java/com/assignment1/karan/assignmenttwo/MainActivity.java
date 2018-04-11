d670382e75b9b81cfe38eb2d35c70e780af01504
Karan-Bhargava <karan.bhargava@metacube.com>
2018-04-11 13:14:08 +0530

    Some minor changes

--- AssignmentTwo/app/src/main/java/com/assignment1/karan/assignmenttwo/MainActivity.java
+++ AssignmentTwo/app/src/main/java/com/assignment1/karan/assignmenttwo/MainActivity.java
@@ -4,37 +4,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
+    Button  button1,button2,button3,button4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LaunchModesDemo.class);
+                view.getContext().startActivity(intent);}
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LaunchModesDemo.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                view.getContext().startActivity(intent);}
+        });
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LaunchModesDemo.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                view.getContext().startActivity(intent);}
+        });
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LaunchModesDemo.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                view.getContext().startActivity(intent);}
+        });
     }
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button1:
-                Intent intent1 = new Intent(this, LaunchModesDemo.class);
-                startActivity(intent1);
-                break;
-            case R.id.button2:
-                Intent intent2 = new Intent(this, LaunchModesDemo.class);
-                intent2.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                startActivity(intent2);
-                break;
-            case R.id.button3:
-                Intent intent3 = new Intent(this, LaunchModesDemo.class);
-                intent3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent3);
-                break;
-            case R.id.button4:
-                Intent intent4 = new Intent(this, LaunchModesDemo.class);
-                intent4.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent4);
-                break;
 
-        }
-    }
 }
