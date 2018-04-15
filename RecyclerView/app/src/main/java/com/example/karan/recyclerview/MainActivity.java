8ba88a1ddceca9d6b3d877ac0d482217140b539b
karan bhargava <karan.bbhargava@metacube.com>
2018-04-15 13:58:55 +0530

    RecyclerView Example with orientation change handling

---
+++ RecyclerView/app/src/main/java/com/example/karan/recyclerview/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.karan.recyclerview;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
+    private GridLayoutManager mGridManager;
+    private String[] myDataset = new String[10];
+    private final Integer SPAN_COUNT = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // initialize the dataset
+        initDataset();
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // handle layout on orientation change
+        mLayoutManager = new LinearLayoutManager(this);
+        mGridManager = new GridLayoutManager(this, SPAN_COUNT);
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            mRecyclerView.setLayoutManager(mLayoutManager);
+        }
+        else {
+            mRecyclerView.setLayoutManager(mGridManager);
+        }
+
+        // specify an adapter
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
+                mLayoutManager.getOrientation());
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+    }
+    private void initDataset() {
+        for(int i=0; i<=9; i++)
+            myDataset[i] = "Dataset " + i;
+    }
+}
